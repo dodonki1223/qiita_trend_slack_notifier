437513365cb7b495f8becc56af8c5d63c2230ce8
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-08-08 14:39:08 +0900

    プロジェクトのみで実行できるようにすべてgemをよしなにやってくれるよう修正

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 
 source 'https://rubygems.org'
 
-gem 'qiita_trend', '~> 0.2.2'
+gem 'qiita_trend', '~> 0.2.8'
 gem 'rubocop', '~> 0.62'
--- notify_trend.rb
+++ notify_trend.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'bundler/setup'
 require 'qiita_trend'
 require './qiita_notifier/qiita_notifier'
 require './qiita_notifier/command_line_arg'
@@ -10,13 +11,19 @@ command_line_args = CommandLineArg.new
 trend_type = command_line_args.get(:target)
 is_new = command_line_args.get(:new)
 
-# Qiita通知クラスを作成
-daily_trend = QiitaTrend::Trend.new(trend_type)
-qiita_notifier = QiitaNotifier::QiitaNotifier.new(QiitaNotifier.configuration.web_hook_url, daily_trend, trend_type)
-
-# Slack通知の実行
-if is_new
-  qiita_notifier.new_item_notify
+# Qiitaのトレンドを取得する
+begin
+  daily_trend = QiitaTrend::Trend.new(trend_type)
+rescue QiitaTrend::Error::LoginFailureError => e
+  puts e.message
+  exit
 else
-  qiita_notifier.item_notify
+  qiita_notifier = QiitaNotifier::QiitaNotifier.new(QiitaNotifier.configuration.web_hook_url, daily_trend, trend_type)
+
+  # Slack通知の実行
+  if is_new
+    qiita_notifier.new_item_notify
+  else
+    qiita_notifier.item_notify
+  end
 end
