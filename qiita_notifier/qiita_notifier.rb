22ba326dcf1f602f39d7da3ff69dbf78b538409d
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-08-08 10:19:34 +0900

    Qiitaの表記と同じになるよう修正

--- qiita_notifier/qiita_notifier.rb
+++ qiita_notifier/qiita_notifier.rb
@@ -33,28 +33,61 @@ module QiitaNotifier
     # blocksを作成する
     def create_blocks(trend_data)
       blocks = trend_data.each_with_object([]) do |item, result|
-        text = SlackBlocks.build_qiita(
-          SlackBlocks.build_qiita_title(item['title'], item['article']),
-          SlackBlocks.build_qiita_content(item['is_new_arrival'], item['user_name'], item['likes_count'])
-        )
+        title = SlackBlocks.build_bold_text(SlackBlocks.build_link_text(item['article'], item['title']))
+        user_link = SlackBlocks.build_link_text(item['user_page'], item['user_name'])
+        post_date = convert_qiita_post_date(item['created_at'])
+        content = SlackBlocks.build_qiita_content(item['is_new_arrival'], user_link, post_date, item['likes_count'])
+
+        text = SlackBlocks.build_qiita(title, content)
         image = SlackClient.new(item['user_image'], true).exists_url? ? item['user_image'] : DEFAULT_USER_IMAGE
 
         result.push SlackBlocks.section_with_image(text, image)
         result.push SlackBlocks.divider
       end
+
+      notify_title = SlackBlocks.build_bold_text(notify_article_time)
+      blocks.insert(0, SlackBlocks.section(notify_title))
+      blocks.insert(1, SlackBlocks.divider)
       blocks
     end
 
     # Slackに通知する
     def notify(blocks)
-      blocks.each_slice(15) do |block|
-        post_data = SlackBlocks.build_post_data('Qiitaトレンド', block)
+      blocks.each_slice(21) do |block|
+        post_data = SlackBlocks.build_post_data("Qiita：#{notify_article_time}", block)
         slack_client = SlackClient.new(@web_hook_url, true)
         request = slack_client.create_post_request(post_data)
         response = slack_client.start { |http| http.request(request) }
 
         puts "#{response.code}：#{response.message}"
       end
+    end
+
+    # 通知するQiitaの時間を取得する
+    def notify_article_time
+      if Time.now.hour >= 5 && Time.now.hour < 17
+        "#{Date.today.strftime('%Y年%m月%d日')}05時のトレンド"
+      elsif Time.now.hour >= 17
+        "#{Date.today.strftime('%Y年%m月%d日')}17時のトレンド"
+      elsif Time.now.hour < 5
+        "#{(Date.today - 1).strftime('%Y年%m月%d日')}17時のトレンド"
+      end
+    end
+
+    # 現在時間から記事の投稿時間をqiitaの表記に変換する
+    def convert_qiita_post_date(date_time)
+      diff = DateTime.now - DateTime.parse(date_time)
+
+      # 求めた差分を秒に変換する(diffはRationalクラス)
+      diff_second = (diff * 24 * 60 * 60).to_i
+
+      # 秒を時間に変換する
+      diff_hour = diff_second / (60 * 60)
+      return "#{diff_hour} hours ago" if diff_hour <= 23
+
+      # 時間を日付に変換する
+      diff_day = diff_hour / 24
+      "#{diff_day} days ago"
     end
   end
 end
--- qiita_notifier/slack_blocks.rb
+++ qiita_notifier/slack_blocks.rb
@@ -6,6 +6,28 @@ module QiitaNotifier
   # blocks用のデータを作成する機能を提供するクラス
   # https://api.slack.com/reference/messaging/blocks
   class SlackBlocks
+    # リンクテキストを作成する
+    def self.build_link_text(link, text)
+      "<#{link}|#{text}>"
+    end
+
+    # 太字のテキストを作成する
+    def self.build_bold_text(text)
+      "*#{text}*"
+    end
+
+    # blocksの`Section`を作成する
+    # https://api.slack.com/tools/block-kit-builder?blocks=%5B%7B%22type%22%3A%22section%22%2C%22text%22%3A%7B%22type%22%3A%22mrkdwn%22%2C%22text%22%3A%22This%20is%20a%20mrkdwn%20section%20block%20%3Aghost%3A%20*this%20is%20bold*%2C%20and%20~this%20is%20crossed%20out~%2C%20and%20%3Chttps%3A%2F%2Fgoogle.com%7Cthis%20is%20a%20link%3E%22%7D%7D%5D
+    def self.section(text)
+      {
+        'type' => 'section',
+        'text' => {
+          'type' => 'mrkdwn',
+          'text' => text
+        }
+      }
+    end
+
     # blocksの`Section with Image`を作成する
     # https://api.slack.com/tools/block-kit-builder?blocks=%5B%7B%22type%22%3A%22section%22%2C%22text%22%3A%7B%22type%22%3A%22mrkdwn%22%2C%22text%22%3A%22Take%20a%20look%20at%20this%20image.%22%7D%2C%22accessory%22%3A%7B%22type%22%3A%22image%22%2C%22image_url%22%3A%22https%3A%2F%2Fapi.slack.com%2Fimg%2Fblocks%2Fbkb_template_images%2Fpalmtree.png%22%2C%22alt_text%22%3A%22palm%20tree%22%7D%7D%5D
     def self.section_with_image(text, image)
@@ -23,15 +45,10 @@ module QiitaNotifier
       }
     end
 
-    # Slack通知用のQiitaタイトルを作成する
-    def self.build_qiita_title(title, article_url)
-      "*<#{article_url}|#{title}>*"
-    end
-
     # Slack通知用のQiita記事内容を作成する
-    def self.build_qiita_content(is_new_arrival, user_name, likes_count)
+    def self.build_qiita_content(is_new_arrival, user, post_date, likes_count)
       new_stamp = is_new_arrival ? ':new:' : ''
-      "#{new_stamp} by #{user_name} :thumbsup: #{likes_count}"
+      "#{new_stamp} by #{user} #{post_date} :thumbsup: #{likes_count}"
     end
 
     # Slack通知用のQiitaの内容（タイトルと記事内容）を作成する
